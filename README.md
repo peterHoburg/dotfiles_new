91adfad9735aea124867a519cfcb5b6f72c2cba5
Peter <peterHoburg@users.noreply.github.com>
2025-01-24 06:55:10 -0500

    started adding more custom help info

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 sudo apt install luarocks
 
-LAZYGIT*VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit*${LAZYGIT_VERSION}\_Linux_x86_64.tar.gz"
+LAZYGIT*VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]_')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}\_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip && cd ~/.local/share/fonts && unzip JetBrainsMono.zip && rm JetBrainsMono.zip && fc-cache -fv
--- nvim/lua/custom-help/help.lua
+++ nvim/lua/custom-help/help.lua
@@ -1,22 +1,42 @@
--- lua/plugins/help.lua
 local CustomHelp = {}
 
+local data = {
+	{ "Keybinding", "Description" },
+	{ "<leader>x", "close buffer" },
+	{ "<leader>v", "split window vertically" },
+	{ "<leader>h", "split window horizontally" },
+	{ "<leader>se", "make split windows equal width & height" },
+	{ "<leader>xs", "close current split window" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+}
+local function format_row(col1, col2)
+	return string.format("%-20s %-40s", col1, col2)
+end
+
 function CustomHelp.show_help_window()
-	-- your floating window code here
+	local text = CustomHelp.generate_text()
+
+	local help_lines = {}
+	for _, row in ipairs(data) do
+		table.insert(help_lines, format_row(row[1], row[2]))
+	end
 	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
-		"This is custom help",
-		"Press <q> to close",
-	})
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, help_lines)
 	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
 
-	-- calculate window size/pos
 	local width = math.floor(vim.o.columns * 0.5)
 	local height = math.floor(vim.o.lines * 0.3)
 	local row = math.floor((vim.o.lines - height) / 2)
 	local col = math.floor((vim.o.columns - width) / 2)
 
-	-- open floating window
 	vim.api.nvim_open_win(buf, true, {
 		style = "minimal",
 		relative = "editor",
